077f66926ec2e6289a320237c642590ba797e962
AchavalNahuel <achaval353@gmail.com>
2025-10-21 12:37:42 -0300

    create users

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ import clientRoutes from "./clientRoutes.js";
 import qrRoutes from "./qrRoutes.js";
 import productRoutes from "./productRoutes.js";
 import categoryRoutes from "./categoryRoutes.js";
+import userRoutes from "./routes/usersRoutes.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/clients", clientRoutes);
 router.use("/qrs", qrRoutes);
 router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/users", userRoutes);
 
 export default router;
